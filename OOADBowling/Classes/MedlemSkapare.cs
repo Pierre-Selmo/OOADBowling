0f888010a47a05708d1473dfbd7c27edff30c671
Pierre Selmö <Pierre_Selmo@outlook.com>
2025-03-27 23:04:10 +0100

    Diagram

--- OOADBowling/Classes/MedlemSkapare.cs
+++ OOADBowling/Classes/MedlemSkapare.cs
@@ -9,11 +9,11 @@ namespace Bowling.Classes
     // MedlemSkapare använder Factory Pattern för att skapa Medlem-objekt
     public static class MedlemSkapare
     {
-        private static int nextId = 1;
+        private static int NextId = 1;
 
         public static Medlem SkapaMedlem(string namn)
         {
-            return new Medlem(namn, nextId++); // Skapar och returnerar ett nytt Medlem-objekt
+            return new Medlem(namn, NextId++); // Skapar och returnerar ett nytt Medlem-objekt
         }
     }
 }
